4e468fa00dfed5d57745d9d365ba4c9d29d88c08
David M. Syzdek <syzdek@bindlebinaries.com>
2011-05-09 21:24:05 -0800

    Adding version information to toggled module API

--- include/toggled_module.h
+++ include/toggled_module.h
@@ -37,5 +37,66 @@
 #ifndef _TOGGLED_MODULE_H
 #define _TOGGLED_MODULE_H 1
 
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+#ifdef PMARK
+#pragma mark -
+#pragma mark Headers
+#endif
+
+#include <toggled.h>
+#include <inttypes.h>
+
+
+///////////////////
+//               //
+//  Definitions  //
+//               //
+///////////////////
+#ifdef PMARK
+#pragma mark -
+#pragma mark Definitions & Macros
+#endif
+
+// toggled module API version info
+#define TOGGLED_MODULE_VERSION_CURRENT    0x00L
+#define TOGGLED_MODULE_VERSION_REVISION   0x00L
+#define TOGGLED_MODULE_VERSION_AGE        0x00L
+
+
+/////////////////
+//             //
+//  Datatypes  //
+//             //
+/////////////////
+#ifdef PMARK
+#pragma mark -
+#pragma mark Datatypes
+#endif
+
+/// @brief toggled module information
+///
+/// Version Info for Modules/Library:
+///    Current    -- the current version number of this API
+///    Revision   -- the revision of the implementation of the API version
+///    Age        -- How many seqential past API versions is supported by
+///                  this implementation
+typedef struct toggled_module_info ToggledModuleInfo;
+struct toggled_module_info
+{
+   uint64_t      toggled_current;    ///< current version of libtoggled_module.la
+   uint64_t      toggled_revision;   ///< revision of libtoggled_module.la
+   uint64_t      toggled_age;        ///< age of libtoggled_module.la
+   uint64_t      module_current;     ///< current version of module's config API
+   uint64_t      module_revision;    ///< revision of module's config API
+   uint64_t      module_age;         ///< age of module's config API
+   const char  * module_name;        ///< name of module
+   const char  * module_description; ///< description of module
+};
+
+
 #endif
 /* end of header */
